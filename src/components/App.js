9c18f3a3e7f36b2470aae683d873dc35f81d1324
shananasim16 <shanashabnam02@gmail.com>
2024-10-02 19:20:33 +0530

    list

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+const data = [{id : 1, name : "abc"}, {id : 2, name : "abc"}, {id : 3, name : "abc"}, {id : 4, name : "abc"},]
+
+
 const App = () => {
+
+
+
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+              <ol id='relativeList' key={"relativeList"}>
+                {data.map((item) => {
+                  return <li id={`relativeListItem${item.id}`} key={`relativeListItem${item.id}`}>{item.name}</li>
+                })}
+              </ol>
     </div>
   )
 }
